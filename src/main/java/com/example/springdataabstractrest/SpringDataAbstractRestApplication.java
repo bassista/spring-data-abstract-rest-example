9323e646400481ad844d163b8106f9fdaeb555a7
Mohit Sinha <mohitsinha251@gmail.com>
2017-08-22 14:02:21 +0530

    First commit

---
+++ src/main/java/com/example/springdataabstractrest/SpringDataAbstractRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdataabstractrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAbstractRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAbstractRestApplication.class, args);
+	}
+}
